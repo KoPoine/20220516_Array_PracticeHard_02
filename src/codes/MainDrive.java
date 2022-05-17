15425d1df48e0a7e197aa1a1a4b056c31b203dac
조경진 <kmc901206@gmail.com>
2022-05-17 09:48:43 +0900

    주석 내용 추가

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MainDrive {
 	public static void main(String[] args) {
 		
+//		1. 컴퓨터의 난수 값 3개를 배열로 받는다.
 		int[] testNum = new int[3];
 		
 		for (int i = 0; i < testNum.length;i++) {
@@ -34,11 +35,14 @@ public class MainDrive {
 		int totalCount = 0;
 		
 		while(true) {
+			
+//			2. 사용자의 3자리 숫자를 받아서 배열로 만들어 준다.
 			System.out.print("답안 제출 : ");
 			int inputNum = myScanner.nextInt();
 			
 			totalCount++; 
 			
+//			2-1 사용자가 입력한 값을 넣을 3칸짜리 배열을 생성 
 			int[] myNum = new int[3];
 			
 //			100의 자리 숫자가 myNum[0]에 들어가도록 역순으로 넣어준다.
@@ -50,6 +54,7 @@ public class MainDrive {
 				}
 			}
 			
+//			3. 컴퓨터가 S / B 계산해주는 로직
 			int sCount = 0;
 			int bCount = 0;
 			
@@ -66,6 +71,8 @@ public class MainDrive {
 				}
 			}
 			
+			
+//			4. S / B 계산값을 출력
 			if (sCount == 3) {
 				System.out.println("삼진입니다. 축하합니다.");
 				break;
